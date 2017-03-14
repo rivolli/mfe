9573daf4e25151078d3e8a2d79d3232eb427b4ef
lpfgarcia <lpfgarcia@gmail.com>
2017-03-14 14:06:17 -0300

    fix createFolds

--- R/internal.R
+++ R/internal.R
@@ -12,30 +12,23 @@ replace.numeric.columns <- function(x) {
   as.data.frame(sapply(x, as.factor))
 }
 
-createFolds <- function(y, k) {
+createFolds <- function(y, folds) {
 
-  if(k > min(table(y))) {
-    k <- min(table(y))
-    warning("k argument must be <= the number of examples in the miniroty
-      class")
+  if(folds <= 1L | folds > min(table(y))) {
+    stop("folds argument must be a integer > 1 and <= the number of examples in 
+      the minority class")
   }
 
   names(y) <- 1:length(y)
   index <- lapply(1:nlevels(y), function(i) {
-    rep(1:k, length.out=length(y[y == levels(y)[i]]))
+    rep(1:folds, length.out=length(y[y == levels(y)[i]]))
   })
 
   index <- unlist(index)
-  folds <- lapply(1:k, function(i) {
+  folds <- lapply(1:folds, function(i) {
     as.numeric(names(y[index == i]))
   })
 
   return(folds)
-}
-
-validation <- function(y) {
-  if(min(table(y)) == 1)
-    return(TRUE)
-  return(FALSE)
 }
 
